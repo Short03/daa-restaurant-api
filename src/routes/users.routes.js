ea97425ad48fb2c78779f913378292774e0ec2ed
Adrian Short <adrian_short97@hotmail.com>
2023-04-20 05:46:20 -0600

    Correcion de carpetas

---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => res.status(200).send('GET /reservations succeede'));
+router.post('/', (req, res) => res.status(201).send('POST /reservations succeede'));
+router.get('/:id', (req, res) => res.status(200).send('GET /reservations/:id succeede'));
+router.delete('/:id', (req, res) => res.status(204).send());
+
+
+module.exports = router;
